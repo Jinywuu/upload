3409c05f06b79078c09df629cc4ff6f751f43fd9
wjy <wjy@qq.com>
2023-03-16 22:06:14 +0800

    Initial commit

---
+++ src/main/java/upload/controller/hellowcentos.java
@@ -0,0 +1,17 @@
+package upload.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Slf4j
+@RestController
+@RequestMapping("/hello")
+public class hellowcentos {
+    @GetMapping("/ss")
+    public String hellocentos(){
+        log.info("收到访问");
+        System.out.println();
+        return null;
+    }
+}
